5e45cd4a3f352d9585273cf8fb0eaffbad0ccd53
Marcos <mani.diaz@alumno.um.edu.ar>
2023-04-27 15:24:07 -0300

    Modificacion

--- Clase 1/Ejercicio3.py
+++ Clase 1/Ejercicio3.py
@@ -7,6 +7,7 @@ parser.add_argument('file', help = 'Es el archivo a leer')
 parser.add_argument('average',nargs = '?', help = 'Promedio de longitud de palabras')
 
 args = parser.parse_args()
+#hola
 
 try:
     with open(args.file) as f:
