a36c36e596fc25f6a8d1f241b69317d3ec2fa750
Marcos <mani.diaz@alumno.um.edu.ar>
2023-06-07 14:16:58 -0300

    Bien, (creo), (puntitos suspensivos)

--- Trabajo Sorpresa/sorpresa.py
+++ Trabajo Sorpresa/sorpresa.py
@@ -1,5 +1,6 @@
 import math
 from concurrent.futures import ThreadPoolExecutor
+import threading
 
 def calculate_term(n, x):
     return ((-1) ** n) * (x ** (2 * n + 1)) / math.factorial(2 * n + 1)
@@ -7,6 +8,10 @@ def calculate_term(n, x):
 def calculate_term_sum(args):
     start, end, x = args
     return sum(calculate_term(i, x) for i in range(start, end))
+
+def calculate_total_sum(results):
+    total_sum = sum(results)
+    return total_sum
 
 def main():
     x = float(input("valor x: "))
@@ -17,9 +22,9 @@ def main():
     terms_per_thread = term_count // thread_count
 
     with ThreadPoolExecutor() as executor:
-        results = executor.map(calculate_term_sum, ((i * terms_per_thread, (i + 1) * terms_per_thread, x) for i in range(thread_count)))
+        results = list(executor.map(calculate_term_sum, ((i * terms_per_thread, (i + 1) * terms_per_thread, x) for i in range(thread_count))))
 
-    total_sum = sum(results)
+    total_sum = calculate_total_sum(results)
 
     print("suma total:", total_sum)
     print("diferencia:", total_sum - reference_value)
